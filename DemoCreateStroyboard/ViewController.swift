85adbe71f9e2e0f9870e97d82f977bc4f2198f4a
Chao Shin <chao.shin@gmail.com>
2018-04-28 23:36:05 +0800

    『簡易說明Xcode』多個Storyboard的建立

--- DemoCreateStroyboard/ViewController.swift
+++ DemoCreateStroyboard/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func buttonPress(_ sender: UIButton) {
+        let secondStroyboard = UIStoryboard(name: "Second", bundle: nil) // 生成要切換的Stroyboard
+        if let controller = secondStroyboard.instantiateViewController(withIdentifier: "SecondView") as? SecondViewController { // 生成切換Stroyboard的controller
+            controller.text = "Page2"
+//            present(controller, animated: true, completion: nil) // 顯示下一個畫面，使用present
+            navigationController?.pushViewController(controller, animated: true) // 顯示下一個畫面，使用push
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
